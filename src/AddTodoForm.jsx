55fde209b479cb788aa5fc46d11c51e4bcbe0b60
nicoleCodeGirl <nicolecodegirl@gmail.com>
2024-11-22 17:49:17 -0800

    submitting week 2

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,16 @@
+function AddTodoForm () {
+    return (
+
+    <form>
+        <label>Title</label>
+        <input id="todoTitle" htmlFor="todoTitle"></input>
+        <button type="submit">Add</button>
+
+    </form>
+    )
+
+}
+
+
+
+export default AddTodoForm;
